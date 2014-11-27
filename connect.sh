7768245a784db691dba250afcb24f8a5fb07fcb9
Rongcui Dong <carldong76@gmail.com>
2014-11-27 15:17:00 -0500

    Adjusted fonts, added some convinient scripts

---
+++ connect.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh dongr2@clark.server.rpi.edu
--- upload.sh
+++ upload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #ssh dongr2@clark.server.rpi.edu
-scp -r _site/* dongr2@clark.server.rpi.edu:/home/43/dongr2/public_html/
+#scp -r _site/* dongr2@clark.server.rpi.edu:/home/43/dongr2/public_html/
+rsync --protocol=29 -az --progress --delete _site/ dongr2@clark.server.rpi.edu:/home/43/dongr2/public_html/
