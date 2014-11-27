b33987e667b551025f77093275bcb5d78c4907ab
Rongcui Dong <carldong76@gmail.com>
2014-11-27 13:29:01 -0500

    Save

--- _includes/repo/00-Getting_Started/main.c
+++ _includes/repo/00-Getting_Started/main.c
@@ -16,7 +16,7 @@ void main() {
   /* PIO0_2 is used by SWD, so disable it */
   *(vp) 0x4000C1C0 = 0xFFFFFFBFUL;	/* PINENABLE0 register */
   /* Set GPIO Direction */
-  *(vp) 0xA0002000 |= 1 << 2;	/* DIR0, set PINIO2 to output */
+  *(vp) 0xA0002000 |= 1 << 2;	/* DIR0, set PIO0_2 to output */
   for(;;) {
     /* Toggle the LED */
     *(vp) 0xA0002300 |= 1 << 2;	/* NOT0 register */
