f57ea4d85639b8e306853485d3fae5ed7fd4b8a9
Rongcui Dong <carldong76@gmail.com>
2014-11-27 12:04:07 -0500

    Working

--- _includes/repo/00-Getting_Started/main.c
+++ _includes/repo/00-Getting_Started/main.c
@@ -3,6 +3,9 @@
    00-Getting_Started.
    Originally posted on:
    http://homepages.rpi.edu/~dongr2/
+
+   Inspired by http://midibel.com/blink0.html, but I rewrote the whole
+   thing
    
    Licence: Do anything with it, I don't care. Public Domain. Just
    remember I don't give any warrenty
@@ -11,14 +14,14 @@
 typedef unsigned int volatile * vp;
 void main() {
   /* PIO0_2 is used by SWD, so disable it */
-  *(vp) 0x4004C1C0 |= 1 << 3;	/* PINENABLE0 register */
+  *(vp) 0x4000C1C0 = 0xFFFFFFBFUL;	/* PINENABLE0 register */
   /* Set GPIO Direction */
   *(vp) 0xA0002000 |= 1 << 2;	/* DIR0, set PINIO2 to output */
   for(;;) {
     /* Toggle the LED */
     *(vp) 0xA0002300 |= 1 << 2;	/* NOT0 register */
-
     volatile long wait = 240000;
     while (wait > 0) --wait;		/* WAIT */
   }
 }
+
